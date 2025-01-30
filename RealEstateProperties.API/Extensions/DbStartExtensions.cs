79a9a0c8cfed0e260ad89145e86f9282d34b85f1
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-01-30 03:17:24 -0500

    :boom: Extension to initialize DB correctly is created

---
+++ RealEstateProperties.API/Extensions/DbStartExtensions.cs
@@ -0,0 +1,52 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace RealEstateProperties.API.Extensions
+{
+  static class DbStartExtensions
+  {
+    enum DbStartType
+    {
+      OpenConnection = 1,
+      EnsureCreated = 2,
+      Migrate = 3
+    }
+
+    public static (Func<Task> OpenConnection, Func<Task> EnsureCreated, Func<Task> Migrate) DbStart<TContext>(this IHost host) where TContext : DbContext
+    {
+      int delay = 0;
+
+      return (() => Connect(DbStartType.OpenConnection), () => Connect(DbStartType.EnsureCreated), () => Connect(DbStartType.Migrate));
+
+      async Task Connect(DbStartType start)
+      {
+        if (delay == 60)
+          throw new NotImplementedException("1 minute passed, failed to connect to DB");
+        AsyncServiceScope scope = host.Services.CreateAsyncScope();
+        TContext context = scope.ServiceProvider.GetRequiredService<TContext>();
+        DatabaseFacade database = context.Database;
+        try
+        {
+          await using (scope.ConfigureAwait(false))
+          {
+            await (start switch
+            {
+              DbStartType.OpenConnection => database.OpenConnectionAsync(),
+              DbStartType.EnsureCreated => database.EnsureCreatedAsync(),
+              DbStartType.Migrate => database.MigrateAsync(),
+              _ => throw new ArgumentOutOfRangeException(nameof(start), $"Not expected direction value: {start}")
+            });
+            delay = 0;
+          }
+        }
+        catch (Exception exception) when (exception is InvalidOperationException || exception is DbException)
+        {
+          await Task.Delay(TimeSpan.FromSeconds(1));
+          Console.WriteLine($"{++delay} seconds have passed, an error occurred, retrying. Connecting...");
+          await Connect(start);
+        }
+      }
+    }
+  }
+}
