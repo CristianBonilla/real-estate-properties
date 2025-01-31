9bcb43153b369beedbb8ad14922afbb66257e1c4
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-01-30 19:16:09 -0500

    :recycle: Refactored the way to get seed images data

--- RealEstateProperties.Domain.Helpers/ConvertionsHelper.cs
+++
@@ -1,8 +0,0 @@
-namespace RealEstateProperties.Domain.Helpers
-{
-  public class ConvertionsHelper
-  {
-    public static byte[] GetStringToBytes(string source, char separator = ',') => Array.ConvertAll(source.Split(separator), byte.Parse);
-    public static byte[][] GetStringToBytes(string[] sources, char separator = ',') => [.. sources.Select(source => GetStringToBytes(source, separator))];
-  }
-}
