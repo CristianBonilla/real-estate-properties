5720775b7ebb8e814dc56716df226daacb8ecaab
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-06 18:34:32 -0500

    :boom: Service error exception filter
    Was created to respond to all cancelled requests

---
+++ RealEstateProperties.API/Filters/ServiceErrorExceptionFilterAttribute.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RealEstateProperties.Contracts.DTO;
+using RealEstateProperties.Contracts.Exceptions;
+
+namespace RealEstateProperties.API.Filters
+{
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+  public class ServiceErrorExceptionFilterAttribute : ExceptionFilterAttribute
+  {
+    public override void OnException(ExceptionContext context)
+    {
+      if (context.Exception is ServiceErrorException exception)
+      {
+        ServiceError? serviceError = exception.ServiceError;
+        context.Result = new ObjectResult(serviceError)
+        {
+          ContentTypes = ["application/json"],
+          StatusCode = serviceError?.StatusCode
+        };
+        context.ExceptionHandled = true;
+      }
+    }
+  }
+}
