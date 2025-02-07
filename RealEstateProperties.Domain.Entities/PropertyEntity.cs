6c9b35688282183a69af733ac67de0bbe93cdc69
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-07 13:29:09 -0500

    :recycle: Updated entities to add required modifier

--- RealEstateProperties.Domain.Entities/PropertyEntity.cs
+++ RealEstateProperties.Domain.Entities/PropertyEntity.cs
@@ -3,12 +3,12 @@ namespace RealEstateProperties.Domain.Entities
   public class PropertyEntity
   {
     public Guid PropertyId { get; set; }
-    public Guid OwnerId { get; set; }
+    public required Guid OwnerId { get; set; }
     public required string Name { get; set; }
     public required string Address { get; set; }
     public required decimal Price { get; set; }
     public int CodeInternal { get; set; }
-    public int Year { get; set; }
+    public required int Year { get; set; }
     public DateTimeOffset Created { get; set; }
     public byte[] Version { get; set; } = null!;
     public OwnerEntity Owner { get; set; } = null!;
--- RealEstateProperties.Domain.Entities/PropertyTraceEntity.cs
+++ RealEstateProperties.Domain.Entities/PropertyTraceEntity.cs
@@ -3,11 +3,11 @@ namespace RealEstateProperties.Domain.Entities
   public class PropertyTraceEntity
   {
     public Guid PropertyTraceId { get; set; }
-    public Guid PropertyId { get; set; }
+    public required Guid PropertyId { get; set; }
     public required string Name { get; set; }
     public required decimal Value { get; set; }
     public required decimal Tax { get; set; }
-    public DateTimeOffset DateSale { get; set; }
+    public required DateTimeOffset DateSale { get; set; }
     public DateTimeOffset Created { get; set; }
     public byte[] Version { get; set; } = null!;
     public PropertyEntity Property { get; set; } = null!;
