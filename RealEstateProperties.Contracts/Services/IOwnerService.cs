b893e5a7e05871db9088e31fc8ae0aadf745f36c
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-07 11:23:50 -0500

    :recycle: Added new functionality to update or add owner photo

--- RealEstateProperties.Contracts/Services/IOwnerService.cs
+++ RealEstateProperties.Contracts/Services/IOwnerService.cs
@@ -5,6 +5,7 @@ namespace RealEstateProperties.Contracts.Services
   public interface IOwnerService
   {
     Task<OwnerEntity> AddOwner(OwnerEntity owner);
+    Task<OwnerEntity> UpdateOwnerPhoto(Guid ownerId, byte[] photo, string photoName);
     Task<OwnerEntity?> FindOwnerById(Guid ownerId);
     IAsyncEnumerable<OwnerEntity> GetOwners();
   }
