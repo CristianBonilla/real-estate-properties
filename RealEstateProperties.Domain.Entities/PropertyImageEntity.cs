bbc7ff8f4a1c44591c3a63f8438f5a9d51b3b0ca
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-07 13:29:09 -0500

    :recycle: Updated entities to add required modifier

--- RealEstateProperties.Domain.Entities/PropertyImageEntity.cs
+++ RealEstateProperties.Domain.Entities/PropertyImageEntity.cs
@@ -3,8 +3,8 @@ namespace RealEstateProperties.Domain.Entities
   public class PropertyImageEntity : PropertyImage
   {
     public Guid PropertyImageId { get; set; }
-    public Guid PropertyId { get; set; }
-    public bool Enabled { get; set; }
+    public required Guid PropertyId { get; set; }
+    public required bool Enabled { get; set; }
     public DateTimeOffset Created { get; set; }
     public byte[] Version { get; set; } = null!;
     public PropertyEntity Property { get; set; } = null!;
