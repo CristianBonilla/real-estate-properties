5e2262ff87934eb821f7403657acc2288fa3d431
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-07 00:17:46 -0500

    :bug: Bug fixed to add existence of user auth

--- RealEstateProperties.Domain/Services/AuthService.cs
+++ RealEstateProperties.Domain/Services/AuthService.cs
@@ -3,6 +3,7 @@ using RealEstateProperties.Domain.Entities.Auth;
 using RealEstateProperties.Domain.Helpers;
 using RealEstateProperties.Infrastructure.Repositories.Auth.Interfaces;
 using RealEstateProperties.Infrastructure.Repositories.RealEstateProperties.Interfaces;
+using System.Linq.Expressions;
 
 namespace RealEstateProperties.Domain.Services
 {
@@ -24,6 +25,8 @@ namespace RealEstateProperties.Domain.Services
     public Task<UserEntity?> FindUserByUsername(string username) => Task.FromResult(_userRepository.Find(user => StringCommonHelper.IsStringEquivalent(user.Username, username)));
 
     public Task<UserEntity?> FindUserByEmail(string email) => Task.FromResult(_userRepository.Find(user => StringCommonHelper.IsStringEquivalent(user.Email, email)));
+
+    public Task<bool> UserExists(Expression<Func<UserEntity, bool>> expression) => Task.FromResult(_userRepository.Exists(expression));
 
     public IAsyncEnumerable<UserEntity> GetUsers()
     {
