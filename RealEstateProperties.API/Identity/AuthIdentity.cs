3e7a9c6bfa779b2642fbe3a5fd19c14261bf6ed1
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-11 23:00:19 -0500

    :boom: Improved functionality for login and user registration

--- RealEstateProperties.API/Identity/AuthIdentity.cs
+++ RealEstateProperties.API/Identity/AuthIdentity.cs
@@ -2,9 +2,10 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
-using AutoMapper;
 using Microsoft.IdentityModel.Tokens;
+using AutoMapper;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using RealEstateProperties.API.Options;
 using RealEstateProperties.Contracts.DTO.Auth;
 using RealEstateProperties.Contracts.DTO.User;
@@ -27,8 +28,7 @@ namespace RealEstateProperties.API.Identity
 
     public async Task<AuthResult> Login(UserLoginRequest userLoginRequest)
     {
-      UserEntity user = await _authService.FindUserByUsername(userLoginRequest.UsernameOrEmail)
-        ?? await _authService.FindUserByEmail(userLoginRequest.UsernameOrEmail)
+      UserEntity user = await _authService.FindUserByUsernameOrEmail(userLoginRequest.UsernameOrEmail)
         ?? throw new ServiceErrorException(HttpStatusCode.Unauthorized, $"User is not registered or is incorrect \"{userLoginRequest.UsernameOrEmail}\"");
       bool userValidPassoword = HashPasswordHelper.Verify(userLoginRequest.Password, user.Password, user.Salt);
       if (!userValidPassoword)
@@ -37,28 +37,23 @@ namespace RealEstateProperties.API.Identity
       return GenerateAuthForUser(user);
     }
 
-    public async Task<AuthResult> Register(UserRegisterRequest userRequest)
+    public async Task<AuthResult> Register(UserRegisterRequest userRegisterRequest)
     {
-      bool existingUser = await UserExists(userRequest);
-      if (!existingUser)
-        throw new ServiceErrorException(HttpStatusCode.Unauthorized, $"User with provided email or username already exists");
-      UserEntity user = _mapper.Map<UserEntity>(userRequest);
+      bool existingUser = await UserExists(userRegisterRequest);
+      if (existingUser)
+        throw new ServiceErrorException(HttpStatusCode.Unauthorized, $"User with provided document Number or username already exists");
+      UserEntity user = _mapper.Map<UserEntity>(userRegisterRequest);
       UserEntity addedUser = await _authService.AddUser(user);
 
       return GenerateAuthForUser(addedUser);
     }
 
-    public async Task<bool> UserExists(UserRegisterRequest userRequest)
-    {
-      bool existingUser = await _authService.UserExists(user => user.Username == userRequest.Username || user.Email == userRequest.Email);
-
-      return existingUser;
-    }
+    public async Task<bool> UserExists(UserRegisterRequest userRegisterRequest) => await _authService.UserExists(userRegisterRequest.DocumentNumber, userRegisterRequest.Username);
 
     private AuthResult GenerateAuthForUser(UserEntity user)
     {
       JwtSecurityTokenHandler tokenHandler = new();
-      byte[] key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+      byte[] key = Encoding.UTF8.GetBytes(_jwtOptions.Secret);
       SecurityTokenDescriptor tokenDescriptor = new()
       {
         Subject = new([
@@ -69,7 +64,7 @@ namespace RealEstateProperties.API.Identity
           new(ClaimTypes.NameIdentifier, user.Username),
           new(ClaimTypes.UserData, UserToJson(user))
         ]),
-        Expires = DateTime.UtcNow.AddDays(7),
+        Expires = DateTime.UtcNow.AddDays(_jwtOptions.ExpiresInDays),
         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
       };
       SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
@@ -77,6 +72,16 @@ namespace RealEstateProperties.API.Identity
       return new(tokenHandler.WriteToken(token), _mapper.Map<UserResponse>(user));
     }
 
-    private static string UserToJson(UserEntity user) => JsonConvert.SerializeObject(user, Formatting.Indented);
+    private static string UserToJson(UserEntity user)
+    {
+      string userJson = JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings
+      {
+        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        DefaultValueHandling = DefaultValueHandling.Ignore,
+        NullValueHandling = NullValueHandling.Ignore
+      });
+
+      return userJson;
+    }
   }
 }
