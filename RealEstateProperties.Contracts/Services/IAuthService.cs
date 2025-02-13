dfa89a5df99ddd4f9a0f4fd2bd20e1f4e870e3f1
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-02-12 22:40:26 -0500

    :recycle: Improved responses to errors from identity service

--- RealEstateProperties.Contracts/Services/IAuthService.cs
+++ RealEstateProperties.Contracts/Services/IAuthService.cs
@@ -5,8 +5,8 @@ namespace RealEstateProperties.Contracts.Services
   public interface IAuthService
   {
     Task<UserEntity> AddUser(UserEntity user);
-    Task<UserEntity?> FindUserById(Guid userId);
-    Task<UserEntity?> FindUserByUsernameOrEmail(string usernameOrEmail);
+    Task<UserEntity> FindUserById(Guid userId);
+    Task<UserEntity> FindUserByUsernameOrEmail(string usernameOrEmail);
     Task<bool> UserExists(string documentNumber, string email);
     IAsyncEnumerable<UserEntity> GetUsers();
   }
